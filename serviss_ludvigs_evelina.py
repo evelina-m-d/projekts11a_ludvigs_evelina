0ac932081a4fe0ce2f29ad9d15d67280d8af0602
ludvigs4 <144446967+ludvigs4@users.noreply.github.com>
2025-04-08 09:41:37 +0300

    Add files via upload

--- serviss_ludvigs_evelina.py
+++ serviss_ludvigs_evelina.py
@@ -22,11 +22,11 @@ class Klients():  #satur datus par katra klienta pierakstu
         self.modelis = modelis
 
     def remontet(self, stundu_skaits): 
-            stundas_likme = 50 #eiro
-            cena = (stundu_skaits * stundas_likme) + self.nepieciesamas_detalas.detalas_cena #aprēķina izmaksas klientam
-            return cena
+        stundas_likme = 50 #eiro
+        cena = (stundu_skaits * stundas_likme) + self.nepieciesamas_detalas.detalas_cena #aprēķina izmaksas klientam
+        return cena
 
-            #butu janonem no noliktavas detala bet to izdomas kad bus kopa salikts
+        #butu janonem no noliktavas detala bet to izdomas kad bus kopa salikts
 
 class Piegadatajs():
     def __init__(self, laiks, min_apjoms, max_apjoms, cena_par_piegadi):
@@ -35,9 +35,15 @@ class Piegadatajs():
         self.max_apjoms = max_apjoms
         self.cena_par_piegadi = cena_par_piegadi
 
-def pasutijums():
+piegadataji = [
+    Piegadatajs(2, 1, 100, 10),
+    Piegadatajs(3, 1, 2, 3),
+    Piegadatajs(4, 1, 120, 12)
+]
+
+def pasutijums(piegadatajs, detala):
     while True:
-        detala = Detala(1, 1, 1, 1)
+        #detala = Detala(None, None, None, None)
 
         detala.nosaukums = input("Ievadiet, kādu detaļu jums vajag pasūtīt: ")
         detala.marka = input("Ievadiet, kādas mašīnas markas detaļa vajadzīga: ")
@@ -45,7 +51,7 @@ def pasutijums():
         detala.detalas_cena = float(input("Ievadiet, kāda ir šīs detaļas cena: "))
         skaits = float(input("Ievadiet detaļu skaitu: "))
         
-        kopeja_cena = detala.detalas_cena * skaits
+        kopeja_cena = detala.detalas_cena * skaits + piegadatajs.cena_par_piegadi
         
         print(f"Kopējā detaļu cena: {kopeja_cena}")
         faila_detala = {
@@ -53,17 +59,28 @@ def pasutijums():
             "marka" : detala.marka,
             "modelis" : detala.modelis,
             "cena" : detala.detalas_cena,
-            "skaits" : skaits
-        }
+            "skaits" : skaits,
+            "piegades diena" : datetime.now() + datetime.timedelta(days= piegadatajs.laiks) #izveido produkta piegādes laiku no pasūtījuma brīža un katra piegādātāja piegādes laika
+        } 
         
         with open('pasutijumi.json', 'a') as f:
-            json.dump(faila_detala)
+            json.dump(faila_detala, f)
         
         if input("Vai vēlaties pievienot vēl detaļas (j/n)? ") == "n":
             break
 
-def pieraksts():
-    pass
+def pieraksts(pasutitajs=Klients()):
+    detalu_saraksts = pasutitajs.nepieciesamas_detalas
+
+    klients = {
+        "pieraksta laiks" : datetime.now(),
+        "mašīnas marka" : pasutitajs.marka,
+        "mašīnas modelis" : pasutitajs.modelis,
+        "nepieciešamās detaļas" : [detalu_saraksts.split(", ")]
+    }
+    
+    with open('pieraksti.json', 'a') as f:
+            json.dump(klients, f)
 
 def noliktava():
     pass
@@ -76,9 +93,15 @@ def izvelne():
         if izvele == "1":
             noliktava()
         if izvele == "2":
-            pieraksts()
+            detala = input("Kādas detaļas jums ir vajadzīgas (atdalīt ar ', '): ")
+            marka = input("Kāda ir jūsu mašīnas marka: ")
+            modelis = input("Kāds ir jūsu mašīnas modelis: ")
+            pieraksts(Klients(detala, marka, modelis))
         if izvele == "3":
-            pasutijums()
+            for i in piegadataji:
+                print(f"Piegādātājs {piegadataji.index(i) + 1} - pasūtījuma cena: {i.cena_par_piegadi}, pasūtījuma laiks: {i.laiks}, minimālais pasūtījuma daudzums: {i.min_apjoms}, maksimālais pasūtījuma daudzums: {i.max_apjoms}")
+            piegadatajs = input(f"Kādu piegādātāju jūs vēlaties: ")
+            pasutijums(piegadataji[0], Detala(None, None, None, None))
         if izvele == "4":
             print("Visu labu!")
             break
